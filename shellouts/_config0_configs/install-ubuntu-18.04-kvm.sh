4bbbe20997adfd6c67f0ca36982d7831bd25e0ff
gary <gary>
2025-03-19 16:44:34 +0800

    rendered and updated 03-19-2025-16-44

--- shellouts/_config0_configs/install-ubuntu-18.04-kvm.sh
+++
@@ -1,8 +0,0 @@
-# "install kvm"
-echo "######################################################################"
-echo "install kvm"
-echo "######################################################################"
-apt-get -y install qemu-kvm libvirt-bin virt-top libguestfs-tools virtinst bridge-utils
-modprobe vhost_net
-lsmod | grep vhost
-echo vhost_net >> /etc/modules
